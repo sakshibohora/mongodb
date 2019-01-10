9c445b982bb5f9381f80124c3dca01be83309a84
sakshibohora <46265763+sakshibohora@users.noreply.github.com>
2019-01-10 16:21:35 +0530

    Add files via upload

---
+++ find.js
@@ -0,0 +1,11 @@
+var mongo = require('mongodb').MongoClient;
+mongo.connect('mongodb://127.0.0.1:27017/learnyoumongo', function(err, db) {
+// db gives access to the database
+var collection = db.collection('parrots');
+collection.find({
+age : {$gt: +process.argv[2]}
+}).toArray(function(err, docs){
+console.log(docs);
+db.close();
+})
+})
